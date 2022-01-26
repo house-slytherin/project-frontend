82397edc378fded2984d577bf8d2fca06c8dc26b
mdreed2 <meshia.d.reed@gmail.com>
2022-01-25 19:49:28 -0600

    Navbar styles and logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -28,9 +28,9 @@ const alwaysOptions = (
 )
 
 const Header = ({ user }) => (
-  <Navbar bg='primary' variant='dark' expand='md'>
+  <Navbar bg='secondary' variant='dark' expand='md'>
     <Navbar.Brand>
-      <Link to='/' style={{ color: '#FFF', textDecoration: 'none' }}>Jotit</Link>
+      <Link to='/' style={{ color: '#FFF', textDecoration: 'none' }}><img src='./../../../public/images/jotit_logo_150px.png' /></Link>
     </Navbar.Brand>
     <Navbar.Toggle aria-controls='basic-navbar-nav' />
     <Navbar.Collapse id='basic-navbar-nav'>
